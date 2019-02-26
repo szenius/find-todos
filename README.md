90467c81c1940d4bd9776226f8cfe7f886b9e0ee
szenius <ting.szeying@gmail.com>
2019-02-26 20:05:08 +0800

    README, refactoring

--- README.md
+++ README.md
@@ -2,12 +2,38 @@
 A simple script to list paths of all files with the keyword `TODO` in their content. This script starts looking for files in its current working directory, and also explores all sub-directories.
 
 ## User Guide
+To use this project, simply place the `find_todos.py` script in any directory from which you want to start looking for files with `TODO`. Then run the following:
+
 ```
 python find_todos.py
 ```
 
+Please note that all of the following will be detected:
+* Files with the word `TODO`, regardless of case
+* Files with the word `TODO`, regardless of usage (even those represent in the code will be detected)
+
 ## Developer's Guide
-### Running Tests
+
+### Installation
+This project was developed in `Python 3.6.5`. Please install an equivalent version.
+
+No external dependencies were used. All libraries used are built-in Python libraries.
+
+### Running Unit Tests
+The default `test_resources/` have test files set up for us to run unit tests on. However, if you wish to run unit tests on your own test files, please modify the `setUp()` method in `test_find_todos.py`:
+
+```python
+def setUp(self):
+    self.test_directory = "./test_resources/" # The source directory where we will start looking for files with TODOs
+    self.files_with_todos = ["test_dir_1/test11.py", "test_dir_1/test12.py", "test_dir_2/test21.py", "test_dir_2/test22.py", "test_dir_3/test31.py"] # File paths leading to files with TODOs. Should be relative to the source directory.
+    self.files_without_todos = ["test_dir_2/test23.py"] # File paths leading to files without TODOs. Should be relative to the source directory.
+```
+
+Once done, run the following.
 ```
 python -m unittest discover
 ```
+
+## Future Work
+* Make into pip package for easy installation
+* Integration tests
--- test_find_todos.py
+++ test_find_todos.py
@@ -4,21 +4,22 @@ from find_todos import find_todos, has_todo
 
 class TestFindTodos(unittest.TestCase):
     def setUp(self):
-        self.files_with_todos = ["./test_resources/test_dir_1/test11.py", "./test_resources/test_dir_1/test12.py", "./test_resources/test_dir_2/test21.py", "./test_resources/test_dir_2/test22.py", "./test_resources/test_dir_3/test31.py"]
-        self.files_without_todos = ["./test_resources/test_dir_2/test23.py"]
+        self.test_directory = "./test_resources/"
+        self.files_with_todos = ["test_dir_1/test11.py", "test_dir_1/test12.py", "test_dir_2/test21.py", "test_dir_2/test22.py", "test_dir_3/test31.py"]
+        self.files_without_todos = ["test_dir_2/test23.py"]
 
     def test_has_todo(self):
         for file in self.files_with_todos:
-            self.assertTrue(has_todo(file), msg="Did not find the TODO in this file!")
+            self.assertTrue(has_todo(self.test_directory + file), msg="Did not find the TODO in this file!")
         for file in self.files_without_todos:
-            self.assertFalse(has_todo(file), msg="Found TODO in this file, but it doesn't exist!")
+            self.assertFalse(has_todo(self.test_directory + file), msg="Found TODO in this file, but it doesn't exist!")
 
     def test_find_todos(self):
         f = io.StringIO()
         with redirect_stdout(f):
-            find_todos("./test_resources/")
+            find_todos(self.test_directory)
         output = f.getvalue().strip().splitlines()
         self.assertEqual(len(self.files_with_todos), len(output), msg="Numbers of expected and actual output lines are different!")
         for file in output:
-            file = file.replace('\\', '/')
+            file = file.replace('\\', '/').replace(self.test_directory, '')
             self.assertTrue(file in self.files_with_todos, msg="{} does not have a TODO".format(file))
